552bf0cf5f6a48e2eb0d109e9469712a5ac5ef28
王佳 <wangjia@cvte.cn>
2015-05-25 21:20:42 +0800

    day5-02-动态链接库

---
+++ day5-02-动态链接库/maxmin.h
@@ -0,0 +1,11 @@
+#ifndef _MAXMIN_H_
+#define _MAXMIN_H_
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+extern int max(int ,int);
+extern int min(int ,int);
+
+#endif
---
+++ day5-02-动态链接库/min.c
@@ -0,0 +1,10 @@
+#include "maxmin.h"
+
+int min(int a , int b)
+{
+    printf("This is min function !!\n");
+    if(a > b){
+	return b;
+    }
+    return a;
+}
